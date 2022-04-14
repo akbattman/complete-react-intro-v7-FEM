aaf0051c334a92131e83235db96aa8f928f96090
Aaron Battman <aaron.k.battman@outlook.com>
2022-04-14 16:45:50 +0100

    prettier added as dev.depd. and script added to yarn

---
+++ adoption-app/src/App.js
@@ -0,0 +1,36 @@
+/* 
+child as rendered within APP
+NB: one-way data flow ##
+child pet now accepting props from parent APP
+*/
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+// parent being rendered
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Iris",
+      animal: "Tortoise",
+      breed: "Greek",
+    }),
+    React.createElement(Pet, {
+      name: "Dolly",
+      animal: "Dog",
+      breed: "Setter",
+    }),
+    React.createElement(Pet, {
+      name: "Nemo",
+      animal: "Fish",
+      breed: "Clown",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
