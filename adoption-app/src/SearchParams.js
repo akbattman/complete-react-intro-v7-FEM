5a308e92cbe5e0beffc88ff43f1333cc21ecc078
Aaron Battman <aaron.k.battman@outlook.com>
2022-04-15 17:19:38 +0100

    ++ form selects /w labels for animals && breeds /w useState hooks again

--- adoption-app/src/SearchParams.js
+++ adoption-app/src/SearchParams.js
@@ -1,9 +1,13 @@
 import { useState } from "react"; //hook
 
+const ANIMALS = ["bird", "reptile", "fish", "dog", "cat"];
+
 const SearchParams = () => {
   // const location = "Emsworth, Hants";
   const { location, setLocation } = useState("");
   const { animal, setAnimal } = useState("");
+  const { breed, setBreed } = useState("");
+  const allBreeds = ["Setter", "Daschund"];
 
   return (
     <div className="search-params">
@@ -18,6 +22,53 @@ const SearchParams = () => {
             placeholder="location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {/* IMPLICIT RETURN - single line return function - (multilined with brackets) */}
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+            {/* {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })} */}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {allBreeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
