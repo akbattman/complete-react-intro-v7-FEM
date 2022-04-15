88eb9048f3e191389cc6877d39a5c869a3ff500d
Aaron Battman <aaron.k.battman@outlook.com>
2022-04-15 14:44:35 +0100

    script added to package for localserve. pet func'comp' moved to own file. refactored all js to jsx

--- adoption-app/src/App.js
+++ adoption-app/src/App.js
@@ -1,35 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me"),
+//     React.createElement(Pet, {
+//       name: "Iris",
+//       animal: "Tortoise",
+//       breed: "Greek",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Dolly",
+//       animal: "Dog",
+//       breed: "Setter",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Nemo",
+//       animal: "Fish",
+//       breed: "Clown",
+//     }),
+//   ]);
+// };
 
-// parent being rendered
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Iris",
-      animal: "Tortoise",
-      breed: "Greek",
-    }),
-    React.createElement(Pet, {
-      name: "Dolly",
-      animal: "Dog",
-      breed: "Setter",
-    }),
-    React.createElement(Pet, {
-      name: "Nemo",
-      animal: "Fish",
-      breed: "Clown",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Iris" animal="Tortoise" breed="Greek" />
+      <Pet name="Dolly" animal="Dog" breed="Setter" />
+      <Pet name="Nemo" animal="Fish" breed="Clown" />
+    </div>
+  );
 };
 
-// ReactDOM.render(React.createElement(App), document.getElementById("root")); now imported at file head
-render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render removed after import at file head
+// render<(React.createElement(App)>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
