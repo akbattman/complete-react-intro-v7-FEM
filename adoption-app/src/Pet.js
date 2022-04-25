cbd9edc63aeac928585de3a8042a0ba99feeaed0
Aaron Battman <aaron.k.battman@outlook.com>
2022-04-25 16:27:59 +0100

    pet comp upstyled

--- adoption-app/src/Pet.js
+++ adoption-app/src/Pet.js
@@ -8,15 +8,37 @@
 //   ]);
 // };
 
-// REMOVED TO REPLACE WITH JSX. IMPORT AUTOMATIC WHEN JSX BUNDLED
+// REMOVED TO REPLACE WITH JSX. IMPORT AUTOMATIC WHEN JSX COMPILES
 
-const Pet = (props) => {
+// const Pet = (props) => {
+// const {name, animal, breed, images, location, id} = props;
+
+// de-structured props
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    // <div>
+    //   <h1>{props.name}</h1>
+    //   <h2>{props.animal}</h2>
+    //   <h2>{props.breed}</h2>
+    // </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {`${animal.charAt(0).toUpperCase() + animal.slice(1)}
+          — ${breed}
+          — ${location}`}
+        </h2>
+      </div>
+    </a>
   );
 };
 
