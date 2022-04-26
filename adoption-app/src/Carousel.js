8a3d40bc0c20b4aca29fcc5b4516fa89f3ee12c3
Aaron Battman <aaron.k.battman@outlook.com>
2022-04-26 16:02:18 +0100

    click handler added in class. dataset index ++

--- adoption-app/src/Carousel.js
+++ adoption-app/src/Carousel.js
@@ -11,6 +11,13 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (e) => {
+    // e.preventDefault;
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state; // instance only assigned individually with contructor ## eg. 5 instances = 5 active .states. Carousel can modify its state.
     const { images } = this.props; // shared as static to all instances ## eg. 5 instances = 1 nonunique prop shared. Props are passed - cannot be modified.
@@ -20,9 +27,12 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            //eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={index}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
